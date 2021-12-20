8dfad5cc3061e539e345f88b25a0f727fa314f58
hamzasedka <hamzawi508@gmail.com>
2021-12-20 13:14:21 +0100

    #Movie-Finder-App-Angular-FireBase v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
-import { GenresComponent } from './genres/genres.component';
-import { UpcomingComponent } from './upcoming/upcoming.component';
+
 import { PopularSeriesComponent } from './popular-series/popular-series.component';
 import { SerieComponent } from './serie/serie.component';
 import { ActorComponent } from './actor/actor.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { SearchComponent } from './search/search.component';
-import { LoginComponent } from './login/login.component';
-import { JSONP_HOME } from '@angular/http/src/backends/browser_jsonp';
+
 import { HomePageComponent } from './home-page/home-page.component';
 import { FilterComponent } from './filter/filter.component';
 
